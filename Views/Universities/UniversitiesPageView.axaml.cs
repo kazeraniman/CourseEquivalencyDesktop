41cc61a81dd0a4249603cfeeba349c7d5ebfbb30
Iman Kazerani <kazeraniman@gmail.com>
2024-08-21 18:35:29 -0400

    Moving the university interaction to the proper view and fixing a bug where the interaction handler registration was not getting wiped on view destroy when using the TransitioningContentControl which would cause the view to no longer work on subsequent navigations.

--- Views/Universities/UniversitiesPageView.axaml.cs
+++ Views/Universities/UniversitiesPageView.axaml.cs
@@ -1,13 +1,54 @@
-﻿using Avalonia;
+﻿using System;
+using System.Threading.Tasks;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using CourseEquivalencyDesktop.Models;
+using CourseEquivalencyDesktop.ViewModels.Universities;
 
 namespace CourseEquivalencyDesktop.Views.Universities;
 
 public partial class UniversitiesPageView : UserControl
 {
+    private IDisposable? createUniversityInteractionDisposable;
+
     public UniversitiesPageView()
     {
         InitializeComponent();
+    }
+
+    protected override void OnUnloaded(RoutedEventArgs e)
+    {
+        base.OnUnloaded(e);
+
+        createUniversityInteractionDisposable?.Dispose();
+    }
+
+    protected override void OnDataContextChanged(EventArgs e)
+    {
+        createUniversityInteractionDisposable?.Dispose();
+        if (DataContext is UniversitiesPageViewModel vm)
+        {
+            createUniversityInteractionDisposable = vm.CreateUniversityInteraction.RegisterHandler(SpawnCreateUniversityWindow);
+        }
+
+        base.OnDataContextChanged(e);
+    }
+
+    private async Task<University?> SpawnCreateUniversityWindow(int? id)
+    {
+        if (TopLevel.GetTopLevel(this) is not Window window)
+        {
+            return null;
+        }
+
+        var createUniversityViewModel = new CreateUniversityViewModel();
+        var createUniversityWindow = new CreateUniversityWindow
+        {
+            DataContext = createUniversityViewModel
+        };
+
+        createUniversityViewModel.OnRequestCloseWindow += (_, args) => createUniversityWindow.Close((args as CreateUniversityViewModel.CreateUniversityEventArgs)?.University);
+
+        return await createUniversityWindow.ShowDialog<University>(window);
     }
 }
