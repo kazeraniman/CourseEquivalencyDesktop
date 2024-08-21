7f5c0d9d5672653a72502a29a2b9c820b96900a6
Iman Kazerani <kazeraniman@gmail.com>
2024-08-21 04:42:24 -0400

    Moving the async initialization code from before the main window is created to after in order to try to alleviate issues that could be caused by something silently hanging before the main window is shown and resulting in the app running as a background task rather than an app.

--- App.axaml.cs
+++ App.axaml.cs
@@ -2,11 +2,8 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
-using CommunityToolkit.Mvvm.DependencyInjection;
-using CourseEquivalencyDesktop.Services;
 using CourseEquivalencyDesktop.ViewModels.General;
 using CourseEquivalencyDesktop.Views.General;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace CourseEquivalencyDesktop;
 
@@ -17,22 +14,8 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override async void OnFrameworkInitializationCompleted()
+    public override void OnFrameworkInitializationCompleted()
     {
-        // Register all the services needed for the application to run
-        var collection = new ServiceCollection();
-        collection.AddCommonServices();
-
-        // Creates a ServiceProvider containing services from the provided IServiceCollection
-        var services = collection.BuildServiceProvider();
-
-        // Make the services generally available
-        Ioc.Default.ConfigureServices(services);
-
-        // Load the settings file
-        var userSettingsService = Ioc.Default.GetRequiredService<UserSettingsService>();
-        await userSettingsService.LoadSettings();
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             // Line below is needed to remove Avalonia data validation.
