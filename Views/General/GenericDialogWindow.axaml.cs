b77f2662b8e55c350c53985384d118fff7c6f897
Iman Kazerani <kazeraniman@gmail.com>
2024-08-26 17:04:08 -0400

    Adding in the generic message dialog window and the service to manage it.

---
+++ Views/General/GenericDialogWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Controls;
+
+namespace CourseEquivalencyDesktop.Views.General;
+
+public partial class GenericDialogWindow : Window
+{
+    #region Constructors
+    public GenericDialogWindow()
+    {
+        InitializeComponent();
+    }
+    #endregion
+}
