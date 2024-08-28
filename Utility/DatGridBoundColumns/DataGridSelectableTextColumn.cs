3496bc8e861c7cb6342ded8140a07d6d69495be4
Iman Kazerani <kazeraniman@gmail.com>
2024-08-28 18:44:00 -0400

    Adding in a custom DataGridBoundColumn to reuse for my text columns. It's basically the same as the regular text column but it allows for the text to be selected and for there to be a tooltip.

---
+++ Utility/DatGridBoundColumns/DataGridSelectableTextColumn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.Styling;
+
+namespace CourseEquivalencyDesktop.Utility.DatGridBoundColumns;
+
+public class DataGridSelectableTextColumn : DataGridBoundColumn
+{
+    #region Constants / Static Readonly
+    private const int MAX_HEIGHT = 30;
+
+    private static readonly Thickness padding = new(10, 5);
+    #endregion
+
+    #region DataGridBoundColumn
+    protected override Control GenerateElement(DataGridCell cell, object dataItem)
+    {
+        var element = new SelectableTextBlock
+        {
+            VerticalAlignment = VerticalAlignment.Center,
+            Padding = padding,
+            MaxHeight = MAX_HEIGHT,
+            Styles =
+            {
+                new Style(ele => ele.Is<SelectableTextBlock>())
+                {
+                    Setters =
+                    {
+                        new Setter(ToolTip.TipProperty, Binding)
+                    }
+                }
+            }
+        };
+
+        if (Binding != null)
+        {
+            element.Bind(TextBlock.TextProperty, Binding);
+        }
+
+        return element;
+    }
+
+    protected override object PrepareCellForEdit(Control editingElement, RoutedEventArgs editingEventArgs)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override Control GenerateEditingElementDirect(DataGridCell cell, object dataItem)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+}
