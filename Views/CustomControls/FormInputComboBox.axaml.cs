25465660b4a71e5f9c6686ff36c3eac4bf1b9871
Iman Kazerani <kazeraniman@gmail.com>
2024-08-28 15:35:20 -0400

    Creating a templated control for labelled comboboxes. Not bothering with the unlabelled version until the need arises. Trying out a new style of templated control where I inherit from the base control and take the entire template from the Avalonia github repo (MIT license). This style is definitely easier in terms of code as I don't need to forward everythin but the AXAML is quite a bit messier, but I should have full control. Maybe I'll migrate the textboxes to this form as well, but that's down the line if need arises.

---
+++ Views/CustomControls/FormInputComboBox.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+
+namespace CourseEquivalencyDesktop.Views.CustomControls;
+
+public class FormInputComboBox : ComboBox
+{
+    #region Avalonia Properties
+    public static readonly StyledProperty<object?> LabelTextProperty =
+        ContentControl.ContentProperty.AddOwner<FormInputTextBox>();
+
+    public static readonly StyledProperty<Geometry?> IconProperty =
+        Path.DataProperty.AddOwner<FormInputTextBox>();
+    #endregion
+
+    #region Properties
+    public object? LabelText
+    {
+        get => GetValue(LabelTextProperty);
+        set => SetValue(LabelTextProperty, value);
+    }
+
+    public Geometry? Icon
+    {
+        get => GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+    #endregion
+
+    #region Templated Control
+    /// <summary>
+    ///     Set the geometry on the path as it is not set by itself despite the binding due to how paths set their initial
+    ///     geometry.
+    ///     See <see cref="Path" /> for more details (and its inheritance from <see cref="Shape" />).
+    /// </summary>
+    /// <param name="e">Event args.</param>
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        Classes.Add("StyledTextBox");
+
+        var path = e.NameScope.Find<Path>("ComboBoxPath");
+        if (path != null)
+        {
+            path.Data = Icon;
+        }
+    }
+    #endregion
+}
