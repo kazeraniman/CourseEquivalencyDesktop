e11d986b433a0bc79f0ab8dc300e98f676015ff9
Iman Kazerani <kazeraniman@gmail.com>
2024-09-16 22:44:43 -0400

    Fixing an issue where the Study Plans button did not have a space in between the words. Extracting some constants while I'm in the neighbourhood.

--- ViewModels/General/MainPageViewModel.cs
+++ ViewModels/General/MainPageViewModel.cs
@@ -15,6 +15,23 @@ namespace CourseEquivalencyDesktop.ViewModels.General;
 
 public partial class MainPageViewModel : BaseViewModel
 {
+    #region Constants
+    private const string HOME_LABEL = "Home";
+    private const string UNIVERSITIES_LABEL = "Universities";
+    private const string COURSES_LABEL = "Courses";
+    private const string EQUIVALENCIES_LABEL = "Equivalencies";
+    private const string STUDENTS_LABEL = "Students";
+    private const string STUDY_PLANS_LABEL = "Study Plans";
+    private const string SETTINGS_LABEL = "Settings";
+    private const string HOME_ICON = "HomeIconData";
+    private const string UNIVERSITY_ICON = "UniversityIconData";
+    private const string COURSES_ICON = "CourseIconData";
+    private const string EQUIVALENCIES_ICON = "EquivalencyIconData";
+    private const string STUDENTS_ICON = "StudentIconData";
+    private const string STUDY_PLANS_ICON = "StudyPlansIconData";
+    private const string SETTINGS_ICON = "SettingsIconData";
+    #endregion
+
     #region Properties
     public NavigationPageInfo[] Pages { get; init; }
 
@@ -31,31 +48,31 @@ public partial class MainPageViewModel : BaseViewModel
         {
             Pages =
             [
-                new NavigationPageInfo("Home", "HomeIconData", new HomePageViewModel()),
-                new NavigationPageInfo("Universities", "UniversityIconData", new UniversitiesPageViewModel()),
-                new NavigationPageInfo("Courses", "CourseIconData", new CoursesPageViewModel()),
-                new NavigationPageInfo("Equivalencies", "EquivalencyIconData", new EquivalenciesPageViewModel()),
-                new NavigationPageInfo("Students", "StudentIconData", new StudentsPageViewModel()),
-                new NavigationPageInfo("StudyPlans", "StudyPlansIconData", new StudyPlansPageViewModel()),
-                new NavigationPageInfo("Settings", "SettingsIconData", new SettingsPageViewModel())
+                new NavigationPageInfo(HOME_LABEL, HOME_ICON, new HomePageViewModel()),
+                new NavigationPageInfo(UNIVERSITIES_LABEL, UNIVERSITY_ICON, new UniversitiesPageViewModel()),
+                new NavigationPageInfo(COURSES_LABEL, COURSES_ICON, new CoursesPageViewModel()),
+                new NavigationPageInfo(EQUIVALENCIES_LABEL, EQUIVALENCIES_ICON, new EquivalenciesPageViewModel()),
+                new NavigationPageInfo(STUDENTS_LABEL, STUDENTS_ICON, new StudentsPageViewModel()),
+                new NavigationPageInfo(STUDY_PLANS_LABEL, STUDY_PLANS_ICON, new StudyPlansPageViewModel()),
+                new NavigationPageInfo(SETTINGS_LABEL, SETTINGS_ICON, new SettingsPageViewModel())
             ];
         }
         else
         {
             Pages =
             [
-                new NavigationPageInfo("Home", "HomeIconData", Ioc.Default.GetRequiredService<HomePageViewModel>()),
-                new NavigationPageInfo("Universities", "UniversityIconData",
+                new NavigationPageInfo(HOME_LABEL, HOME_ICON, Ioc.Default.GetRequiredService<HomePageViewModel>()),
+                new NavigationPageInfo(UNIVERSITIES_LABEL, UNIVERSITY_ICON,
                     Ioc.Default.GetRequiredService<UniversitiesPageViewModel>()),
-                new NavigationPageInfo("Courses", "CourseIconData",
+                new NavigationPageInfo(COURSES_LABEL, COURSES_ICON,
                     Ioc.Default.GetRequiredService<ServiceCollectionExtensions.CoursesPageViewModelFactory>()(null)),
-                new NavigationPageInfo("Equivalencies", "EquivalencyIconData",
+                new NavigationPageInfo(EQUIVALENCIES_LABEL, EQUIVALENCIES_ICON,
                     Ioc.Default.GetRequiredService<EquivalenciesPageViewModel>()),
-                new NavigationPageInfo("Students", "StudentIconData",
+                new NavigationPageInfo(STUDENTS_LABEL, STUDENTS_ICON,
                     Ioc.Default.GetRequiredService<StudentsPageViewModel>()),
-                new NavigationPageInfo("StudyPlans", "StudyPlansIconData",
+                new NavigationPageInfo(STUDY_PLANS_LABEL, STUDY_PLANS_ICON,
                     Ioc.Default.GetRequiredService<StudyPlansPageViewModel>()),
-                new NavigationPageInfo("Settings", "SettingsIconData",
+                new NavigationPageInfo(SETTINGS_LABEL, SETTINGS_ICON,
                     Ioc.Default.GetRequiredService<SettingsPageViewModel>())
             ];
         }
